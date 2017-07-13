96d7ec9a48718cc2fc134cd103c420c6a2c5593f
Jeff Baker <sinerasis@gmail.com>
2017-07-13 10:11:41 -0400

    add option to go straight to option from command line, some logic updates, make sure colors reset properly, if setting foreground, must set background (users default colors in mind)

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,161 +7,165 @@ using System.Reflection;
 
 namespace ConsoleApp1
 {
-    class Program
-    {
-        // this is the main method for our program
-        static void Main(string[] args = null)
-        {
-            Console.ForegroundColor = ConsoleColor.White;
-            // capture the users choice            
-            string choice = MainMenu();
+	class Program
+	{
+		// this is the main method for our program
+		static void Main(string[] args = null)
+		{
+            // clear output
+            Console.Clear();
 
-            // do the selected choice
-            switch (choice)
+            // set foreground/background
+			Console.BackgroundColor = ConsoleColor.Black;
+			Console.ForegroundColor = ConsoleColor.White;
+
+            // allows skipping the main menu on startup and go directly to your choice if called via the cmd line
+            string choice = "";
+            if (args == null || args.Length < 1)
             {
-                case "1":
-                    OutputRandomNumber();
-                    break;
-
-                case "2":
-                    OutputNCharsNTimes();
-                    break;
-                default:
-                    Oops(choice + " is not 1 or 2...", "Main");
-                    break;
-            }
-
-            // repeat or not
-            if (Again())
-            {
-                string[] empty = { "" };
-                Main(empty);
-            }
-        }
-
-        // asks the user if they want to do it all over again, returns bool
-        static bool Again()
-        {
-            // output question, read input
-            Console.WriteLine("Would you like to do it again?");
-            string choice = Console.ReadLine();
-
-            if (!choice.IsNullOrEmpty() && choice.ToLower().Substring(0, 1) == "n")
-            {
-                return false;
+                choice = MainMenu();
             }
             else
             {
-                if (choice.IsNullOrEmpty() || choice.ToLower().Substring(0, 1) != "y")
-                {
-                    Oops(choice + " seems like the wrong answer", "Again");
-                }
-                else
-                {
-                    return true;
-                }
+                // capture the users choice
+                choice = args[0];
             }
-            return true;
-        }
+			
+			// do the selected choice
+			switch (choice)
+			{
+				case "1":
+					OutputRandomNumber();
+					break;
 
-        // outputs the main menu and returns the users choice
-        static string MainMenu()
-        {
-            // output question
-            Console.WriteLine("Please make a selection:");
+				case "2":
+					OutputNCharsNTimes();
+					break;
 
-            // array of choices
-            string[] selections = {
-                "Output a Random Number",
-                "Output n Characters n Times"
-            };
+				default:
+                    // if an invalid option is input, dump the user back at the main menu
+					Main();
+					break;
+			}
 
-            // output choices
-            int i = 1;
-            foreach (string selection in selections)
-            {
-                Console.WriteLine(i + ": " + selection);
-                i++;
-            }
+			// repeat or not
+			if (Again())
+			{
+				Main();
+			}
+		}
 
-            // capture user choice
-            string choice = Console.ReadLine();
-            if (!choice.IsNullOrEmpty() || (choice.ToLower().Substring(0, 1) != "1" || choice.ToLower().Substring(0, 1) != "2"))
-            {
-                Oops("Invalid Option", "MainMenu");
-            }
-            // return choice
-            return choice;
-        }
+		// asks the user if they want to do it all over again, returns bool
+		static bool Again()
+		{
+			// output question, read input
+			Console.WriteLine("Would you like to do it again?");
+			string choice = Console.ReadLine();
 
-        // outputs a random number
-        static void OutputRandomNumber()
-        {
-            try
-            {
-                // instantiate random class
-                Random rand = new Random();
+			if (!choice.IsNullOrEmpty() && choice.ToLower().Substring(0, 1) == "n")
+			{
+				return false;
+			}
+			return true;
+		}
 
-                // output random number
-                Console.WriteLine(rand.Next());
+		// outputs the main menu and returns the users choice
+		static string MainMenu()
+		{
+			// output question
+			Console.WriteLine("Please make a selection:");
 
-                // wait for user (just a pause)
-                Console.ReadLine();
-            }
-            catch (Exception ex)
-            {
-                Oops(ex.Message, null);
-            }
-        }
+			// array of choices
+			string[] selections = {
+				"Output a Random Number",
+				"Output n Characters n Times"
+			};
 
-        // outputs N characters, N times
-        static void OutputNCharsNTimes()
-        {
-            // ask user to enter number, capture
-            Console.WriteLine("Enter a number:");
-            string n = Console.ReadLine();
+			// output choices
+			int i = 1;
+			foreach (string selection in selections)
+			{
+				Console.WriteLine(i + ": " + selection);
+				i++;
+			}
 
-            // convert to int
-            int num = Int32.Parse(n);
+			// capture user choice
+			string choice = Console.ReadLine();
+			if (!choice.IsNullOrEmpty() || (choice.ToLower().Substring(0, 1) != "1" || choice.ToLower().Substring(0, 1) != "2"))
+			{
+				Oops("Invalid Option", "MainMenu");
+			}
+			// return choice
+			return choice;
+		}
 
-            // array of characters to pick from
-            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-            // instantiate random class
-            Random rand = new Random();
-
-            // loop n times and output string
-            for (int i = 0; i < num; i++)
-            {
-                // i found this here: https://stackoverflow.com/questions/1344221/how-can-i-generate-random-alphanumeric-strings-in-c
-                string result = new string(Enumerable.Repeat(chars, num)
-                    .Select(s => s[rand.Next(s.Length)]).ToArray());
-
-                Console.WriteLine(result);
-            }
-
-            // wait for user (just a pause)
-            Console.ReadLine();
-        }
-
-        private static void Oops(string msg = null, string method = null)
-        {
+		// outputs a random number
+		static void OutputRandomNumber()
+		{
             Console.Clear();
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Ya Big Dummeh!");
-            if (msg != null)
-            {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine(msg);
-                Console.ForegroundColor = ConsoleColor.White;
-            }
-            if (method != null)
-            {
-                var mi = typeof(Program).GetMethod(method);
-                if (mi != null)
-                {
-                    mi.Invoke(null, null);
-                }
-            }
-        }
-    }
+
+			try
+			{
+				// instantiate random class
+				Random rand = new Random();
+
+				// output random number
+				Console.WriteLine("Random Number: " + rand.Next());
+			}
+			catch (Exception ex)
+			{
+				Oops(ex.Message, null);
+			}
+		}
+
+		// outputs N characters, N times
+		static void OutputNCharsNTimes()
+		{
+            Console.Clear();
+
+			// ask user to enter number, capture
+			Console.WriteLine("Enter a number:");
+			string n = Console.ReadLine();
+
+			// convert to int
+			int num = Int32.Parse(n);
+
+			// array of characters to pick from
+			const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+			// instantiate random class
+			Random rand = new Random();
+
+			// loop n times and output string
+			for (int i = 0; i < num; i++)
+			{
+				// i found this here: https://stackoverflow.com/questions/1344221/how-can-i-generate-random-alphanumeric-strings-in-c
+				string result = new string(Enumerable.Repeat(chars, num)
+					.Select(s => s[rand.Next(s.Length)]).ToArray());
+
+				Console.WriteLine(result);
+			}
+		}
+
+		private static void Oops(string msg = null, string method = null)
+		{
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.WriteLine("Ya Big Dummeh!");
+			if (msg != null)
+			{
+				Console.ForegroundColor = ConsoleColor.Yellow;
+				Console.WriteLine(msg);
+				Console.ForegroundColor = ConsoleColor.Red;
+			}
+			if (method != null)
+			{
+				var mi = typeof(Program).GetMethod(method);
+				if (mi != null)
+				{
+					mi.Invoke(null, null);
+				}
+			}
+
+			Console.ForegroundColor = ConsoleColor.White;
+		}
+	}
 }
